069b82b5803e925e652cc36b202006349f381e5b
Deniz TOPAY <deniztopay@gmail.com>
2024-01-15 18:10:17 +0300

    Completed 02

--- 02_repeatString/repeatString.js
+++ 02_repeatString/repeatString.js
@@ -1,5 +1,16 @@
-const repeatString = function() {
-
+const repeatString = function(string , count) {
+   
+    if ( count == 0){
+        return "";
+    }
+    if ( count < 0){
+        return "ERROR";
+    }
+    let appendString = string;
+    for (let i = 1 ; i < count ; i++){
+        string = string.concat(appendString);
+    }
+    return string;
 };
 
 // Do not edit below this line
