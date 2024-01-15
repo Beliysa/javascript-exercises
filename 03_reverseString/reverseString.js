5e51d7352edf006fd9bb5ce7b334ad1746285ba9
Deniz TOPAY <deniztopay@gmail.com>
2024-01-15 20:39:20 +0300

    Completed 03

--- 03_reverseString/reverseString.js
+++ 03_reverseString/reverseString.js
@@ -1,6 +1,15 @@
-const reverseString = function() {
+const reverseString = function(string) {
+    let len = string.length-1;
+    let temp = "";
 
-};
+    while (  0 <= len ){
+       temp = temp.concat(string[len]);
+       len--;
+    }
+
+    return temp;
+}
+    
 
 // Do not edit below this line
 module.exports = reverseString;
