6057eef31ef6fd5416698291d432d1fad5f9ec1b
Deniz TOPAY <deniztopay@gmail.com>
2024-01-16 18:10:40 +0300

    Completed 04

--- 04_removeFromArray/removeFromArray.js
+++ 04_removeFromArray/removeFromArray.js
@@ -1,5 +1,22 @@
-const removeFromArray = function() {
+const removeFromArray = function(array) {
+    
+    let len = array.length;
+    let argCount = arguments.length;
+    
+    
+    for(let y = 1; y < argCount ; y++){
+        for (let i = 0 ; i < len ; i++){
+            if ( array[i] === arguments[y] ){
+                array.splice(i, 1);
+            }
+        }
 
+    }
+
+   
+    
+    return array;
+     
 };
 
 // Do not edit below this line
